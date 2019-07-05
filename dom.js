0899e216e75c267218e4e2210a922f2665ce3285
Esckep <nuwan@seneview.com>
2019-07-05 15:13:02 +0530

    Included modal HTML

---
+++ dom.js
@@ -0,0 +1,33 @@
+const fancyHTML = `
+<div class="modal fade fm fm-success st-modal-success  show" id="" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" style="display: block;">
+    <div class="modal-dialog modal-dialog-zoom" role="document">
+        <div class="modal-content border-0">					
+            <div class="modal-header py-0 border-0 pt-1">
+                <button type="button" class="close" data-dismiss="modal" aria-lab="" el="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-3 icon-sector"> 
+                            <div class="d-flex align-items-center justify-content-center h-100">
+                                <div class="st-icon d-flex justify-content-center align-items-center rounded-circle">
+                                    <i class="fas fa-check"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-9 content-sector">
+                            <h5>Item Removed</h5>								
+                            <p class="text-muted">Item has Removed from your cart successfully</p>	
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer bg-light rounded-bottom mt-4">
+                <button data-id="fm-cancelButton" id="" type="button" class="btn btn-outline-secondary " data-dismiss="modal">Ok</button>
+            </div>
+        </div>
+    </div>
+</div>
+`;
